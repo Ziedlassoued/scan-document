e4714babd8d9a2e800e45ddada22642bed38ef01
ziedlassoued <zied.lassoued@outlook.de>
2021-11-11 10:11:57 +0100

    :sparkles: Add AddDocumentForm.stories.tsx

---
+++ src/app/components/AddDocumentForm/AddDocumentForm.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AddDocumentForm from './AddDocumentForm';
+
+export default {
+  component: AddDocumentForm,
+  title: 'Components/AddDocumentForm',
+};
+export const Default = () => <AddDocumentForm text="text" />;
