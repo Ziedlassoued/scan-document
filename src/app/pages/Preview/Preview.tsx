0c9b4665f3c86e0429dc966c92c4850c407464f1
ziedlassoued <zied.lassoued@outlook.de>
2021-11-12 15:28:31 +0100

    :lipstick: Add Preview.module.css

--- src/app/pages/Preview/Preview.tsx
+++ src/app/pages/Preview/Preview.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import DocPreview from '../../components/DocPreview/DocPreview';
+import styles from './Preview.module.css';
 
 function Preview(): JSX.Element {
   return (
-    <>
+    <div className={styles.container}>
       <h2>my document</h2>
-      <DocPreview title={'title'} />
-    </>
+      <ul>
+        <li>
+          <p>
+            <DocPreview title={'title'} />
+          </p>
+        </li>
+      </ul>
+    </div>
   );
 }
 
